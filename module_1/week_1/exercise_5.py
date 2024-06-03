f265a1aed36c4e78f1e5b66e511547c00e185b74
HeisoSicaro <heisosicaro1703@gmail.com>
2024-06-04 00:40:28 +0700

    [BUGFIX] M1-W1: remove abs of Mean Difference

--- module_1/week_1/exercise_5.py
+++ module_1/week_1/exercise_5.py
@@ -3,7 +3,7 @@ def md_nre_single_sample(y: float, y_hat: float, n: int, p: int):
     y_mean = y ** (1 / n)
     y_hat_mean = y_hat ** (1 / n)
 
-    return abs(y_mean - y_hat_mean) ** p
+    return (y_mean - y_hat_mean) ** p
 
 if __name__ == "__main__":
     print(md_nre_single_sample(y=100, y_hat=99.5, n=2, p=1))
